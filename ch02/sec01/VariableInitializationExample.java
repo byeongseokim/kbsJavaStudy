85391dc36fc6c9b3b0ff854ffc93d3f6678edcc7
byeongseokim <kbs970318@gmail.com>
2023-04-18 19:57:56 +0900

    연산의 우선순위와 데이터 변환

---
+++ ch02/sec01/VariableInitializationExample.java
@@ -0,0 +1,15 @@
+package ch02.sec01;
+
+public class VariableInitializationExample {
+	public static void main(String[]args) {
+		//변수 value 선언
+		int value=0;
+		
+		//연산 결과를 변수 result의 초기값으로 대입
+		int result = value + 10;
+		
+		//변수 result 값을 읽고 콘솔에 출력
+		System.out.println(result);
+	}
+
+}
