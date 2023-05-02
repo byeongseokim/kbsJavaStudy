f7f2061e3ca08181de77a806c44fcf4cb4445b21
byeongseokim <kbs970318@gmil.com>
2023-05-02 18:20:19 +0900

    Generic

---
+++ ch13/sec02/exam02/CarAgency.java
@@ -0,0 +1,12 @@
+package ch13.sec02.exam02;
+
+public class CarAgency implements Rentable<Car> {
+
+	@Override
+	public Car rent() {
+		// TODO Auto-generated method stub
+		return new Car();
+	}
+	
+
+}
---
+++ ch13/sec02/exam02/Rentable.java
@@ -0,0 +1,6 @@
+package ch13.sec02.exam02;
+
+public interface Rentable<P> {
+	P rent();
+
+}
