eb7cb2e2cd4b7e7ed6a03417df70df9c216d1601
byeongseokim <kbs970318@gmil.com>
2023-05-04 09:06:50 +0900

    503@DESKTOP-JV4AIGK MINGW64 /d/kbs_data/git_data (master) $ cd

---
+++ ch13/sec03/exam01/Box.java
@@ -0,0 +1,16 @@
+package ch13.sec03.exam01;
+
+public class Box<T> {
+	//필드
+	private T t;
+
+	//Getter 메소드
+	public T get() {
+		return t;
+	}
+    //Setter 메소드
+	public void set(T t) {
+		this.t = t;
+	}
+	
+}
---
+++ ch13/sec03/exam01/GenericExample.java
@@ -0,0 +1,24 @@
+package ch13.sec03.exam01;
+
+public class GenericExample {
+	// 제네릭 메소드
+	public static <T> Box<T> boxing(T t) {
+		Box<T> box = new Box<T>();
+		box.set(t);
+		return box;
+
+	}
+
+	public static void main(String[] args) {
+		// 제네릭 메소드 호출
+		Box<Integer> box1 = boxing(100);
+		int intValue = box1.get();
+		System.out.println(intValue);
+
+		// 제네릭 메소드 호출
+		Box<String> box2 = boxing("홍길동");
+		String strValue = box2.get();
+		System.out.println(strValue);
+	}
+
+}
