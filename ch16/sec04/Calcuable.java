dab7b2a93955b82767aae8bfeaab11115a3c918a
byeongseokim <kbs970318@gmil.com>
2023-05-08 16:28:15 +0900

    Lambda

---
+++ ch16/sec04/Calcuable.java
@@ -0,0 +1,7 @@
+package ch16.sec04;
+
+@FunctionalInterface
+public interface Calcuable {
+	double calc(double x, double y);
+
+}
---
+++ ch16/sec04/LambdaExample.java
@@ -0,0 +1,31 @@
+package ch16.sec04;
+
+public class LambdaExample {
+	public static void main(String[] args) {
+		Person person = new Person();
+
+		// 실행문이 두 개 이상일 경우
+		person.action((x, y) -> {
+			double result = x + y;
+			return result;
+
+		});
+		// 리턴문이 하나만 있을 경우(연산식)
+		// Person.actrion((x, y) -> {
+		// return (x, y);
+		// });
+		person.action((x, y) -> (x + y));
+
+		// 리턴문이 하나만 있을 경우(메소드 호출)
+		// Person.actrion((x, y) -> {
+		// return (x, y);
+		// });
+		person.action((x, y) -> sum(x, y));
+	}
+
+	public static double sum(double x, double y) {
+		return (x + y);
+
+	}
+
+}
